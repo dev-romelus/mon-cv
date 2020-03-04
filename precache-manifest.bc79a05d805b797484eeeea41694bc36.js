9811241087be991f301a3795e0da039fb38394eb
Romelus Jean-Marie <romelusjeanmarie.pro@gmail.com>
2020-03-04 17:08:40 +0100

    Updates

---
+++ precache-manifest.bc79a05d805b797484eeeea41694bc36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a8c0542e594ea3878cdef693b83764",
+    "url": "/mon-cv/index.html"
+  },
+  {
+    "revision": "151220d7d5aff8867d8c",
+    "url": "/mon-cv/static/css/main.b1ebc2f7.chunk.css"
+  },
+  {
+    "revision": "12142f4293dd955eafb0",
+    "url": "/mon-cv/static/js/2.542b2d0f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mon-cv/static/js/2.542b2d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151220d7d5aff8867d8c",
+    "url": "/mon-cv/static/js/main.0c4dfeec.chunk.js"
+  },
+  {
+    "revision": "1fffd0194c1f67150ab6",
+    "url": "/mon-cv/static/js/runtime-main.b09b2d77.js"
+  }
+]);
