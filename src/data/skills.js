b811d60f15f8b942a607c78acd953f7de7003dbf
Romelus Jean-Marie <romelusjeanmarie.pro@gmail.com>
2020-03-25 13:21:49 +0100

    new skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,6 +1,6 @@
 export const skills = {
     langages: ['JavaScript', 'TypeScript','SQL','NoSQL','Python','CSS & SASS', 'GraphQL'],
-    frameworks: ['Node.js', 'React.js', 'D3.js', 'Amcharts.js','Express.js'],
+    frameworks: ['Node.js', 'React.js', 'D3.js', 'Amcharts.js','Express.js', 'Anime.js', 'Motion UI'],
     outils: ['MongoDB', 'PostgreSQL', 'Elastisearch','Git & Github', 'Postman'],
     qualite: ['Autonome', 'Curieux', 'Perséverant', 'Courageux','esprit d\'équipe']
 }
